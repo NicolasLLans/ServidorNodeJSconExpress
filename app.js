e00a588ac8d8fb52596a8b2ee6bd099c2391afed
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-05-15 01:08:38 -0300

    Mi Primer servidor hecho con Express

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+
+const { infoCursos } = require('./cursos.js');
+
+//Routing
+
+app.get('/',(req,res)=>{
+      res.send('Mi primer servidor con Express. Cursos 💻.');
+});
+
+app.get('/api/cursos',(req,res)=>{
+      res.send(JSON.stringify(infoCursos));
+});
+
+app.get('/api/cursos/programacion', (req,res)=>{
+      res.send(JSON.stringify(infoCursos.programacion));
+})
+
+//Cursos de programación
+
+app.get('/api/cursos/programacion/:lenguaje', (req,res)=>{
+      const lenguaje = req.params.lenguaje;
+      const resultados = infoCursos.programacion.filter(cursos => cursos.lenguaje === lenguaje);
+      if(resultados.length === 0){
+            return res.status(404).send(`No se encontraron cursos de: ${lenguaje}`);
+      }
+      res.send(JSON.stringify(resultados));
+})
+
+app.get('/api/cursos/programacion/:lenguaje/:nivel', (req,res) => {
+      const lenguaje = req.params.lenguaje;
+      const nivel = req.params.nivel;
+
+      const resultados = infoCursos.programacion.filter(curso => curso.lenguaje && curso.nivel === nivel);
+      if(resultados.length === 0){
+            return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
+      }
+      res.send(JSON.stringify(resultados));
+})
+
+//Cursos de matemáticas
+
+app.get('/api/cursos/matematicas/:tema',(req,res)=>{
+      const tema = req.params.tema;
+      const resultados = infoCursos.matematicas.filter(curso => curso.tema === tema);
+      if(resultados.length === 0){
+            return res.status(404).send(`No se encontraron cursos de ${tema}`);
+      }
+      res.send(JSON.stringify(resultados));
+})
+
+app.get('/api/cursos/matematicas', (req,res)=>{
+      res.send(infoCursos.matematicas);
+})
+
+const PUERTO = process.env.PORT || 3000;
+
+app.listen(PUERTO, () =>{
+      console.log(`El servidor esta escuchando en el puerto ${PUERTO}...`);
+});
---
+++ cursos.js
@@ -0,0 +1,43 @@
+let infoCursos={
+      programacion:[
+            {
+                  id:1,
+                  titulo:'Python básico',
+                  lenguaje:'python',
+                  vistas:13553,
+                  nivel:'Básico'
+            },
+            {
+                  id:2,
+                  titulo:'Python intermedio',
+                  lenguaje:'python',
+                  vistas:13555,
+                  nivel:'intermedio'
+            },
+            {
+                  id:3,
+                  titulo:'Aprende JavaScript',
+                  lenguaje:'javascript',
+                  vistas:10553,
+                  nivel:'Básico'
+            }
+      ],
+      matematicas:[
+            {
+                  id:1,
+                  titulo:'Aprende Calculo',
+                  tema:'calculo',
+                  vistas:12553,
+                  nivel:'Básico'
+            },
+            {
+                  id:2,
+                  titulo:'Algebra',
+                  tema:'algebra',
+                  vistas:18553,
+                  nivel:'intermedio'
+            }
+      ]
+}
+
+module.exports.infoCursos = infoCursos;
